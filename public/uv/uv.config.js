fb5170b321cfbc3ac6a44817fcf71c223daa9db2
jerrygoodman218 <jerrygoodman218@gmail.com>
2025-05-16 22:46:37 -0400

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,12 +1,11 @@
-// This file overwrites the stock UV config.js
-
 self.__uv$config = {
-  prefix: "/uv/service/",
-  encodeUrl: Ultraviolet.codec.xor.encode,
-  decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: "/uv/uv.handler.js",
-  client: "/uv/uv.client.js",
-  bundle: "/uv/uv.bundle.js",
-  config: "/uv/uv.config.js",
-  sw: "/uv/uv.sw.js",
+    prefix: '/uv/service/',
+    bare: '/bare/',
+    encodeUrl: Ultraviolet.codec.base64.encode,
+    decodeUrl: Ultraviolet.codec.base64.decode,
+    handler: '/uv/uv.handler.js',
+    client: '/uv/uv.client.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
 };
