57dba5d05cf0a7d66a73dd5ff17c19d7427d4aac
jerrygoodman218 <jerrygoodman218@gmail.com>
2025-05-16 22:44:17 -0400

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,3 @@
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", () => self.clients.claim());
-
 importScripts("/uv/uv.bundle.js");
 
 const sw = new UVServiceWorker();
